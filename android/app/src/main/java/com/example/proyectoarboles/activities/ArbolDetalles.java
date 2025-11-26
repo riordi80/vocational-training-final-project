f1a2cf35f54b191312966d89567b13a148a09e34
Enrique36247 <enriqueprzg4@gmail.com>
2025-11-26 17:47:39 +0000

    feat(andorid): implements activities(ArbolDetalles, Login, Registrer)

---
+++ android/app/src/main/java/com/example/proyectoarboles/activities/ArbolDetalles.java
@@ -0,0 +1,26 @@
+package com.example.proyectoarboles.activities;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.proyectoarboles.R;
+
+public class ArbolDetalles extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_arbol_detalles);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
