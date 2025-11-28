7f3677d57e396b73723c34ea577af57a64b16c7a
Enrique36247 <enriqueprzg4@gmail.com>
2025-11-28 16:54:43 +0000

    fixed(adnroid): fixed ArbolDetalles

--- android/app/src/main/java/com/example/proyectoarboles/activities/ArbolDetalles.java
+++ android/app/src/main/java/com/example/proyectoarboles/activities/ArbolDetalles.java
@@ -1,6 +1,8 @@
 package com.example.proyectoarboles.activities;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -18,5 +20,18 @@ public class ArbolDetalles extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_arbol_detalles);
 
+        TextView tvNombre = findViewById(R.id.textViewNombreDetalle);
+        TextView tvEspecie = findViewById(R.id.textViewEspecieDetalle);
+        TextView tvFecha = findViewById(R.id.textViewFechaDetalle);
+        TextView tvUbicacion = findViewById(R.id.textViewUbicacion);
+        TextView tvTemp = findViewById(R.id.textViewTemp);
+        TextView tvHumedad = findViewById(R.id.textViewHumedad);
+        TextView tvCO2 = findViewById(R.id.textViewCO2);
+        TextView tvHumedadSuelo = findViewById(R.id.textViewHumedadSuelo);
+
+        Intent intent = getIntent();
+
+        if (intent != null){
+        }
     }
 }
